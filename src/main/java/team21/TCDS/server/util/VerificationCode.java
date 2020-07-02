739393a47c5743d37d6e2ad030ef03dd2ac4f818
LiuPiPiPi <liu_bihao@163.com>
2020-07-02 16:02:19 +0800

    update server util v3.0

--- src/main/java/team21/TCDS/server/util/VerificationCode.java
+++ src/main/java/team21/TCDS/server/util/VerificationCode.java
@@ -8,43 +8,43 @@ import java.io.IOException;
 import java.io.OutputStream;
 
 /**
- * 
  * <h2>随机验证码封装类</h2>
  * <p>
  * 该类用于封装一个随机验证码图片及其内容。要获得验证码图片，请使用saveTo方法将其存入本地路径或是写入输出流。
  * 要获得验证码图片代表的正确内容，请使用getCode方法获得内容字符串。要获得图片对象本身，请调用getImage方法。
  * </p>
+ *
  * @author 高级软件21组(team21)
  * @version 1.0
  */
 public class VerificationCode {
 
-	private String code;
-	private BufferedImage image;
+    private String code;
+    private BufferedImage image;
 
-	public String getCode() {
-		return code.toLowerCase();
-	}
+    public String getCode() {
+        return code.toLowerCase();
+    }
 
-	public void setCode(String code) {
-		this.code = code;
-	}
+    public void setCode(String code) {
+        this.code = code;
+    }
 
-	public BufferedImage getImage() {
-		return image;
-	}
+    public BufferedImage getImage() {
+        return image;
+    }
 
-	public void setImage(BufferedImage image) {
-		this.image = image;
-	}
+    public void setImage(BufferedImage image) {
+        this.image = image;
+    }
 
-	public void saveTo(String path) throws IOException {
-		File f = new File(path);
-		ImageIO.write(image, "jpeg", new FileOutputStream(f));
-	}
-	
-	public void saveTo(OutputStream out) throws IOException {
-		ImageIO.write(image, "jpeg", out);
-	}
+    public void saveTo(String path) throws IOException {
+        File f = new File(path);
+        ImageIO.write(image, "jpeg", new FileOutputStream(f));
+    }
+
+    public void saveTo(OutputStream out) throws IOException {
+        ImageIO.write(image, "jpeg", out);
+    }
 
 }
--- src/main/java/team21/TCDS/server/util/VideoTranscodeUtil.java
+++ src/main/java/team21/TCDS/server/util/VideoTranscodeUtil.java
@@ -16,91 +16,87 @@ import java.util.HashMap;
 import java.util.Map;
 
 /**
- * 
  * <h2>视频转码工具</h2>
  * <p>
  * 该工具用于进行视频转码操作，使用Spring IOC容器管理。
  * </p>
- * 
+ *
  * @author 高级软件21组(team21)
  * @version 1.0
  */
 @Component
 public class VideoTranscodeUtil {
 
-	private EncodingAttributes ea;// 转码设定
-	@Resource
-	private FileBlockUtil fbu;
-	@Resource
-	private NodeMapper nm;
-	@Resource
-	private TCDSFFMPEGLocator kfl;
+    private EncodingAttributes ea;// 转码设定
+    @Resource
+    private FileBlockUtil fbu;
+    @Resource
+    private NodeMapper nm;
+    @Resource
+    private TCDSFFMPEGLocator kfl;
 
-	public static Map<String, VideoTranscodeThread> videoTranscodeThreads = new HashMap<>();
+    public static Map<String, VideoTranscodeThread> videoTranscodeThreads = new HashMap<>();
 
-	{
-		AudioAttributes audio = new AudioAttributes();
-		audio.setCodec("libmp3lame");
-		audio.setBitRate(128000);
-		audio.setChannels(2);
-		audio.setSamplingRate(44100);
-		VideoAttributes video = new VideoAttributes();
-		video.setCodec("libx264");
-		ea = new EncodingAttributes();
-		ea.setFormat("MP4");
-		ea.setVideoAttributes(video);
-		ea.setAudioAttributes(audio);
-	}
+    {
+        AudioAttributes audio = new AudioAttributes();
+        audio.setCodec("libmp3lame");
+        audio.setBitRate(128000);
+        audio.setChannels(2);
+        audio.setSamplingRate(44100);
+        VideoAttributes video = new VideoAttributes();
+        video.setCodec("libx264");
+        ea = new EncodingAttributes();
+        ea.setFormat("MP4");
+        ea.setVideoAttributes(video);
+        ea.setAudioAttributes(audio);
+    }
 
-	/**
-	 * 
-	 * <h2>获取指定视频转码进度</h2>
-	 * <p>
-	 * 以百分制返回指定ID的视频转码进度，如若未开始则自动开始。
-	 * </p>
-	 * 
-	 * @author 高级软件21组(team21)
-	 * @param fId
-	 *            java.lang.String 转码视频的ID
-	 * @return java.lang.String 转码进度，按百分制返回，例如“1.2”代表完成1.2%，返回null则参数不正确。
-	 * @throws Exception
-	 *             获取失败
-	 */
-	public String getTranscodeProcess(String fId) throws Exception {
-		synchronized (videoTranscodeThreads) {
-			VideoTranscodeThread vtt = videoTranscodeThreads.get(fId);
-			Node n = nm.queryById(fId);
-			File f = fbu.getFileFromBlocks(n);
-			if (vtt != null) {
-				if ("FIN".equals(vtt.getProgress())) {
-					String md5 = DigestUtils.md5Hex(new FileInputStream(f));
-					if (md5.equals(vtt.getMd5())
-							&& new File(ConfigureReader.instance().getTemporaryfilePath(), vtt.getOutputFileName())
-									.isFile()) {
-						return vtt.getProgress();
-					} else {
-						videoTranscodeThreads.remove(fId);
-					}
-				} else {
-					return vtt.getProgress();
-				}
-			}
-			String suffix = n.getFileName().substring(n.getFileName().lastIndexOf(".") + 1).toLowerCase();
-			switch (suffix) {
-			case "mp4":
-			case "webm":
-			case "mov":
-			case "avi":
-			case "wmv":
-			case "mkv":
-			case "flv":
-				break;
-			default:
-				throw new IllegalArgumentException();
-			}
-			videoTranscodeThreads.put(fId, new VideoTranscodeThread(f, ea, kfl));
-			return "0.0";
-		}
-	}
+    /**
+     * <h2>获取指定视频转码进度</h2>
+     * <p>
+     * 以百分制返回指定ID的视频转码进度，如若未开始则自动开始。
+     * </p>
+     *
+     * @param fId java.lang.String 转码视频的ID
+     * @return java.lang.String 转码进度，按百分制返回，例如“1.2”代表完成1.2%，返回null则参数不正确。
+     * @throws Exception 获取失败
+     * @author 高级软件21组(team21)
+     */
+    public String getTranscodeProcess(String fId) throws Exception {
+        synchronized (videoTranscodeThreads) {
+            VideoTranscodeThread vtt = videoTranscodeThreads.get(fId);
+            Node n = nm.queryById(fId);
+            File f = fbu.getFileFromBlocks(n);
+            if (vtt != null) {
+                if ("FIN".equals(vtt.getProgress())) {
+                    String md5 = DigestUtils.md5Hex(new FileInputStream(f));
+                    if (md5.equals(vtt.getMd5())
+                            && new File(ConfigureReader.instance().getTemporaryfilePath(), vtt.getOutputFileName())
+                            .isFile()) {
+                        return vtt.getProgress();
+                    } else {
+                        videoTranscodeThreads.remove(fId);
+                    }
+                } else {
+                    return vtt.getProgress();
+                }
+            }
+            String suffix = n.getFileName().substring(n.getFileName().lastIndexOf(".") + 1).toLowerCase();
+            switch (suffix) {
+                case "mp4":
+                case "webm":
+                case "mov":
+                case "avi":
+                case "wmv":
+                case "mkv":
+                case "flv":
+                    break;
+                default:
+                    throw new IllegalArgumentException();
+            }
+            videoTranscodeThreads.put(fId, new VideoTranscodeThread(f, ea, kfl));
+            return "0.0";
+        }
+    }
 
 }

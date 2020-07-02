2495e7bd3732e9138a284699b2dc0d817b2c7082
LiuPiPiPi <liu_bihao@163.com>
2020-07-02 16:00:11 +0800

    update server util

--- src/main/java/team21/TCDS/server/util/NoticeUtil.java
+++ src/main/java/team21/TCDS/server/util/NoticeUtil.java
@@ -13,94 +13,91 @@ import javax.annotation.Resource;
 import java.io.*;
 
 /**
- * 
  * <h2>简述</h2>
  * <p>
  * 详细功能
  * </p>
- * 
+ *
  * @author 高级软件21组(team21)
  * @version 1.0
  */
 @Component
 public class NoticeUtil {
 
-	private String md5;// 公告信息的md5值，如未生成则返回null
-	private MutableDataHolder options;// markdown解析器的参数设置
-	/**
-	 * 程序主目录中的公告文件名称
-	 */
-	public static final String NOTICE_FILE_NAME = "notice.md";
+    private String md5;// 公告信息的md5值，如未生成则返回null
+    private MutableDataHolder options;// markdown解析器的参数设置
+    /**
+     * 程序主目录中的公告文件名称
+     */
+    public static final String NOTICE_FILE_NAME = "notice.md";
 
-	public static final String NOTICE_OUTPUT_NAME = "notice.html";
+    public static final String NOTICE_OUTPUT_NAME = "notice.html";
 
-	@Resource
-	private LogUtil lu;
-	@Resource
-	private TxtCharsetGetter tcg;
+    @Resource
+    private LogUtil lu;
+    @Resource
+    private TxtCharsetGetter tcg;
 
-	public NoticeUtil() {
-		options = new MutableDataSet();
-		options.setFrom(ParserEmulationProfile.MARKDOWN);
-	}
+    public NoticeUtil() {
+        options = new MutableDataSet();
+        options.setFrom(ParserEmulationProfile.MARKDOWN);
+    }
 
-	/**
-	 * 
-	 * <h2>载入公告文件</h2>
-	 * <p>
-	 * 该方法将寻找并尝试载入位于主目录内的“notice.md”文件，如果该文件存在，
-	 * 则会将该文件以markdown格式转化为HTML文档并存入临时目录中，并命名为“notice.html”，
-	 * 同时计算原文件的md5值以便通过该对象的md5字段获取。如已生成旧的“notice.html”文件， 则执行该方法将会覆盖它。
-	 * </p>
-	 * 
-	 * @author 高级软件21组(team21)
-	 */
-	public void loadNotice() {
-		File noticeMD = new File(ConfigureReader.instance().getPath(), NOTICE_FILE_NAME);
-		File noticeHTML = new File(ConfigureReader.instance().getTemporaryfilePath(), NOTICE_OUTPUT_NAME);// 转化后的输出位置
-		if (noticeMD.isFile() && noticeMD.canRead()) {
-			Printer.instance.print("正在载入公告信息...");
-			try {
-				// 先判断公告信息文件的编码格式
-				String inputFileEncode = tcg.getTxtCharset(new FileInputStream(noticeMD));
-				// 将其转化为HTML格式并保存
-				Parser parser = Parser.builder(options).build();
-				HtmlRenderer renderer = HtmlRenderer.builder(options).build();
-				BufferedReader reader = new BufferedReader(
-						new InputStreamReader(new FileInputStream(noticeMD), inputFileEncode));
-				BufferedWriter writer = new BufferedWriter(
-						new OutputStreamWriter(new FileOutputStream(noticeHTML), "UTF-8"));
-				String line = null;
-				while ((line = reader.readLine()) != null) {
-					String html = renderer.render(parser.parse(line));
-					writer.write(html);
-					writer.newLine();
-				}
-				reader.close();
-				writer.flush();
-				writer.close();
-				// 计算md5并保存
-				md5 = DigestUtils.md5Hex(new FileInputStream(noticeMD));
-				Printer.instance.print("公告信息载入完成。");
-				return;
-			} catch (Exception e) {
-				Printer.instance.print("错误：公告文件载入失败，服务器将无法为用户显示公告内容。");
-			}
-		}
-		md5 = null;
-	}
+    /**
+     * <h2>载入公告文件</h2>
+     * <p>
+     * 该方法将寻找并尝试载入位于主目录内的“notice.md”文件，如果该文件存在，
+     * 则会将该文件以markdown格式转化为HTML文档并存入临时目录中，并命名为“notice.html”，
+     * 同时计算原文件的md5值以便通过该对象的md5字段获取。如已生成旧的“notice.html”文件， 则执行该方法将会覆盖它。
+     * </p>
+     *
+     * @author 高级软件21组(team21)
+     */
+    public void loadNotice() {
+        File noticeMD = new File(ConfigureReader.instance().getPath(), NOTICE_FILE_NAME);
+        File noticeHTML = new File(ConfigureReader.instance().getTemporaryfilePath(), NOTICE_OUTPUT_NAME);// 转化后的输出位置
+        if (noticeMD.isFile() && noticeMD.canRead()) {
+            Printer.instance.print("正在载入公告信息...");
+            try {
+                // 先判断公告信息文件的编码格式
+                String inputFileEncode = tcg.getTxtCharset(new FileInputStream(noticeMD));
+                // 将其转化为HTML格式并保存
+                Parser parser = Parser.builder(options).build();
+                HtmlRenderer renderer = HtmlRenderer.builder(options).build();
+                BufferedReader reader = new BufferedReader(
+                        new InputStreamReader(new FileInputStream(noticeMD), inputFileEncode));
+                BufferedWriter writer = new BufferedWriter(
+                        new OutputStreamWriter(new FileOutputStream(noticeHTML), "UTF-8"));
+                String line = null;
+                while ((line = reader.readLine()) != null) {
+                    String html = renderer.render(parser.parse(line));
+                    writer.write(html);
+                    writer.newLine();
+                }
+                reader.close();
+                writer.flush();
+                writer.close();
+                // 计算md5并保存
+                md5 = DigestUtils.md5Hex(new FileInputStream(noticeMD));
+                Printer.instance.print("公告信息载入完成。");
+                return;
+            } catch (Exception e) {
+                Printer.instance.print("错误：公告文件载入失败，服务器将无法为用户显示公告内容。");
+            }
+        }
+        md5 = null;
+    }
 
-	/**
-	 * 
-	 * <h2>获取公告信息的md5标识</h2>
-	 * <p>
-	 * 该方法将返回公告的md5标识，从而帮助前端判定是否需要显示公告。如果没有公告，那么该方法会返回null。
-	 * </p>
-	 * 
-	 * @author 高级软件21组(team21)
-	 * @return java.lang.String md5字符串或null。
-	 */
-	public String getMd5() {
-		return md5;
-	}
+    /**
+     * <h2>获取公告信息的md5标识</h2>
+     * <p>
+     * 该方法将返回公告的md5标识，从而帮助前端判定是否需要显示公告。如果没有公告，那么该方法会返回null。
+     * </p>
+     *
+     * @return java.lang.String md5字符串或null。
+     * @author 高级软件21组(team21)
+     */
+    public String getMd5() {
+        return md5;
+    }
 }
--- src/main/java/team21/TCDS/server/util/PowerPoint2PDFUtil.java
+++ src/main/java/team21/TCDS/server/util/PowerPoint2PDFUtil.java
@@ -24,115 +24,114 @@ import java.util.Arrays;
 import java.util.List;
 
 /**
- * 
  * <h2>PPT转PDF工具</h2>
  * <p>
  * 该工具是实现PPT在线预览功能的核心，能将ppt/pptx格式的文档以流形式转化为PDF。如果该文档中存在系统未安装的字体，则使用开源免费字体“文泉驿正黑”替代显示，以确保在非Windows系统下也能正确转换。该工具应由Spring
  * IOC容器管理。
  * </p>
- * 
+ *
  * @author 高级软件21组(team21)
  * @version 1.0
  */
 @Component
 public class PowerPoint2PDFUtil {
-	
-	/**
-	 * 
-	 * <h2>执行PPT格式转换（ppt/pptx）</h2>
-	 * <p>将输入流中的PPT文件转换为PDF格式并输出至指定输出流，该方法线程阻塞。</p>
-	 * @author 高级软件21组(team21)
-	 * @param in java.io.InputStream 输入流，输入ppt/pptx格式
-	 * @param out java.io.OutputStream 输出流，输出PDF格式
-	 * @param type team21.TCDS.server.enumeration.PowerPointType 使用何种标准进行解析（ppt/pptx）
-	 */
-	public void convertPdf(InputStream in, OutputStream out, PowerPointType type) throws Exception {
-		double zoom = 2;
-		AffineTransform at = new AffineTransform();
-		at.setToScale(zoom, zoom);
-		Document pdfDocument = new Document();
-		PdfWriter pdfWriter = PdfWriter.getInstance(pdfDocument, out);
-		PdfPTable table = new PdfPTable(1);
-		pdfWriter.open();
-		pdfDocument.open();
-		Dimension pgsize = null;
-		Image slideImage = null;
-		BufferedImage img = null;
-		if (type.equals(PowerPointType.PPT)) {
-			SlideShow ppt = new SlideShow(in);
-			in.close();
-			pgsize = ppt.getPageSize();
-			Slide slide[] = ppt.getSlides();
-			pdfDocument.setPageSize(new Rectangle((float) pgsize.getWidth(), (float) pgsize.getHeight()));
-			pdfWriter.open();
-			pdfDocument.open();
-			for (int i = 0; i < slide.length; i++) {
-				TextRun[] truns = slide[i].getTextRuns();
-				for (int k = 0; k < truns.length; k++) {
-					RichTextRun[] rtruns = truns[k].getRichTextRuns();
-					for (int l = 0; l < rtruns.length; l++) {
-						if(Arrays.asList(GraphicsEnvironment.getLocalGraphicsEnvironment().getAvailableFontFamilyNames()).contains(rtruns[l].getFontName())) {
-							continue;
-						}
-						rtruns[l].setFontIndex(1);
-						rtruns[l].setFontName("WenQuanYi Zen Hei");
-					}
-				}
 
-				img = new BufferedImage((int) Math.ceil(pgsize.width * zoom), (int) Math.ceil(pgsize.height * zoom),
-						BufferedImage.TYPE_INT_RGB);
-				Graphics2D graphics = img.createGraphics();
-				graphics.setTransform(at);
+    /**
+     * <h2>执行PPT格式转换（ppt/pptx）</h2>
+     * <p>将输入流中的PPT文件转换为PDF格式并输出至指定输出流，该方法线程阻塞。</p>
+     *
+     * @param in   java.io.InputStream 输入流，输入ppt/pptx格式
+     * @param out  java.io.OutputStream 输出流，输出PDF格式
+     * @param type team21.TCDS.server.enumeration.PowerPointType 使用何种标准进行解析（ppt/pptx）
+     * @author 高级软件21组(team21)
+     */
+    public void convertPdf(InputStream in, OutputStream out, PowerPointType type) throws Exception {
+        double zoom = 2;
+        AffineTransform at = new AffineTransform();
+        at.setToScale(zoom, zoom);
+        Document pdfDocument = new Document();
+        PdfWriter pdfWriter = PdfWriter.getInstance(pdfDocument, out);
+        PdfPTable table = new PdfPTable(1);
+        pdfWriter.open();
+        pdfDocument.open();
+        Dimension pgsize = null;
+        Image slideImage = null;
+        BufferedImage img = null;
+        if (type.equals(PowerPointType.PPT)) {
+            SlideShow ppt = new SlideShow(in);
+            in.close();
+            pgsize = ppt.getPageSize();
+            Slide slide[] = ppt.getSlides();
+            pdfDocument.setPageSize(new Rectangle((float) pgsize.getWidth(), (float) pgsize.getHeight()));
+            pdfWriter.open();
+            pdfDocument.open();
+            for (int i = 0; i < slide.length; i++) {
+                TextRun[] truns = slide[i].getTextRuns();
+                for (int k = 0; k < truns.length; k++) {
+                    RichTextRun[] rtruns = truns[k].getRichTextRuns();
+                    for (int l = 0; l < rtruns.length; l++) {
+                        if (Arrays.asList(GraphicsEnvironment.getLocalGraphicsEnvironment().getAvailableFontFamilyNames()).contains(rtruns[l].getFontName())) {
+                            continue;
+                        }
+                        rtruns[l].setFontIndex(1);
+                        rtruns[l].setFontName("WenQuanYi Zen Hei");
+                    }
+                }
 
-				graphics.setPaint(Color.white);
-				graphics.fill(new Rectangle2D.Float(0, 0, pgsize.width, pgsize.height));
-				slide[i].draw(graphics);
-				graphics.getPaint();
-				slideImage = Image.getInstance(img, null);
-				table.addCell(new PdfPCell(slideImage, true));
-			}
-		}
-		if (type.equals(PowerPointType.PPTX)) {
-			GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
-			XMLSlideShow ppt = new XMLSlideShow(in);
-			pgsize = ppt.getPageSize();
-			XSLFSlide slide[] = ppt.getSlides();
-			pdfDocument.setPageSize(new Rectangle((float) pgsize.getWidth(), (float) pgsize.getHeight()));
-			pdfWriter.open();
-			pdfDocument.open();
+                img = new BufferedImage((int) Math.ceil(pgsize.width * zoom), (int) Math.ceil(pgsize.height * zoom),
+                        BufferedImage.TYPE_INT_RGB);
+                Graphics2D graphics = img.createGraphics();
+                graphics.setTransform(at);
 
-			for (int i = 0; i < slide.length; i++) {
-				for (XSLFShape shape : slide[i].getShapes()) {
-					if (shape instanceof XSLFTextShape) {
-						XSLFTextShape txtshape = (XSLFTextShape) shape;
-						for (XSLFTextParagraph textPara : txtshape.getTextParagraphs()) {
-							List<XSLFTextRun> textRunList = textPara.getTextRuns();
-							for (XSLFTextRun textRun : textRunList) {
-								if (Arrays.stream(ge.getAvailableFontFamilyNames()).parallel()
-										.anyMatch((e) -> e.equals(textRun.getFontFamily()))) {
-									continue;
-								}
-								textRun.setFontFamily("WenQuanYi Zen Hei");
-							}
-						}
-					}
-				}
-				img = new BufferedImage((int) Math.ceil(pgsize.width * zoom), (int) Math.ceil(pgsize.height * zoom),
-						BufferedImage.TYPE_INT_RGB);
-				Graphics2D graphics = img.createGraphics();
-				graphics.setTransform(at);
-				graphics.setPaint(Color.white);
-				graphics.fill(new Rectangle2D.Float(0, 0, pgsize.width, pgsize.height));
-				slide[i].draw(graphics);
+                graphics.setPaint(Color.white);
+                graphics.fill(new Rectangle2D.Float(0, 0, pgsize.width, pgsize.height));
+                slide[i].draw(graphics);
+                graphics.getPaint();
+                slideImage = Image.getInstance(img, null);
+                table.addCell(new PdfPCell(slideImage, true));
+            }
+        }
+        if (type.equals(PowerPointType.PPTX)) {
+            GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
+            XMLSlideShow ppt = new XMLSlideShow(in);
+            pgsize = ppt.getPageSize();
+            XSLFSlide slide[] = ppt.getSlides();
+            pdfDocument.setPageSize(new Rectangle((float) pgsize.getWidth(), (float) pgsize.getHeight()));
+            pdfWriter.open();
+            pdfDocument.open();
 
-				graphics.getPaint();
-				slideImage = Image.getInstance(img, null);
-				table.addCell(new PdfPCell(slideImage, true));
-			}
-		}
-		pdfDocument.add(table);
-		pdfDocument.close();
-		pdfWriter.close();
-	}
+            for (int i = 0; i < slide.length; i++) {
+                for (XSLFShape shape : slide[i].getShapes()) {
+                    if (shape instanceof XSLFTextShape) {
+                        XSLFTextShape txtshape = (XSLFTextShape) shape;
+                        for (XSLFTextParagraph textPara : txtshape.getTextParagraphs()) {
+                            List<XSLFTextRun> textRunList = textPara.getTextRuns();
+                            for (XSLFTextRun textRun : textRunList) {
+                                if (Arrays.stream(ge.getAvailableFontFamilyNames()).parallel()
+                                        .anyMatch((e) -> e.equals(textRun.getFontFamily()))) {
+                                    continue;
+                                }
+                                textRun.setFontFamily("WenQuanYi Zen Hei");
+                            }
+                        }
+                    }
+                }
+                img = new BufferedImage((int) Math.ceil(pgsize.width * zoom), (int) Math.ceil(pgsize.height * zoom),
+                        BufferedImage.TYPE_INT_RGB);
+                Graphics2D graphics = img.createGraphics();
+                graphics.setTransform(at);
+                graphics.setPaint(Color.white);
+                graphics.fill(new Rectangle2D.Float(0, 0, pgsize.width, pgsize.height));
+                slide[i].draw(graphics);
+
+                graphics.getPaint();
+                slideImage = Image.getInstance(img, null);
+                table.addCell(new PdfPCell(slideImage, true));
+            }
+        }
+        pdfDocument.add(table);
+        pdfDocument.close();
+        pdfWriter.close();
+    }
 
 }
